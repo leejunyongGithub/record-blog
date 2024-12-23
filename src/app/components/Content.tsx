7e17b5957b660296ac889c5d507925b259bcb010
junyong <junyong0751@gmail.com>
2024-12-23 15:57:37 +0900

    [feat] footer 추가

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -1,3 +1,3 @@
 export default function Content({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return <div className="flex flex-col gap-4 h-[calc(100vh-172px)]">{children}</div>;
 }
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,14 @@
-export default function Footer(){
-    return <footer></footer>
+"use client";
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+
+export default function Footer() {
+  return (
+    <footer className="h-[100px] flex flex-col gap-2 items-center justify-center p-4">
+      <Link href="https://github.com/leejunyongGithub" target="_blank">
+        <BsGithub size={24} />
+      </Link>
+      <span className="text-gray-400">Copyright © 2024, All right reserved.</span>
+    </footer>
+  );
 }
